25038c3c6dec32ba2fe4259a6f1cd9c4491ec6d0
esimbulan <195094745+esimbulan@users.noreply.github.com>
2025-01-29 01:16:07 +0800

    Add 04 exercise solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayValue, ...toRemove) {
+    arrayValue.forEach((element,index) => {
+        if (toRemove.includes(element)) {
+            arrayValue.splice(index, 1);
+        }
+    });
+    return arrayValue;
 };
 
 // Do not edit below this line
