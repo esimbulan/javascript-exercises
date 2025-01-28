d40ba2ad82da06b43971c6115dae1f5c550705dd
esimbulan <195094745+esimbulan@users.noreply.github.com>
2025-01-29 00:47:24 +0800

    Add 01 and 02 exercise solutin

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(toPrint, length) {
+    if (length < 0) {
+        return "ERROR";
+    }
+    let toPrintRepeated = "";
+    for(let i = 0; i < length; i++) {
+        toPrintRepeated += toPrint;
+    }
+    return toPrintRepeated;
 };
 
 // Do not edit below this line
